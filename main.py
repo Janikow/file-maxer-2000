9263135af2e35fa9bb9d29797c6de8e0faa87a2b
Tem <160335817+Janikow@users.noreply.github.com>
2026-09-16 15:04:29 -0500

    nyheheheh

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
 import subprocess
+import glob
+import os
+
+def count_copies():
+    copies = glob.glob("copy_*.py")
+    return len(copies)
 
 def replicate():
     with open(sys.argv[0], 'r') as current_file:
         my_code = current_file.read()
 
-    number_of_copies = 3
+    number_of_copies = 2
 
     for i in range(1, number_of_copies + 1):
         filename = f"copy_{i}.py"
@@ -19,3 +25,4 @@ def replicate():
 
 if __name__ == "__main__":
     replicate()
+    count_copies()
